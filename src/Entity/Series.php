45f66691bf4e34302eeb6bef4fd54d60eaf2e186
Otavio Henrique de Lima e Silva <otaviohlsilva@gmail.com>
2024-04-25 14:22:37 -0300

    commit 1

---
+++ src/Entity/Series.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\SeriesRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+#[ORM\Entity(repositoryClass: SeriesRepository::class)]
+class Series
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private int $id;
+
+    
+    public function __construct(
+        // #[ORM\Column(length: 255)]
+        #[ORM\Column]
+        #[Assert\NotBlank]
+        #[Assert\Length(min: 5)]
+        private string $name = '',
+    ) {}
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+}
